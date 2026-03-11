1b3fdb4b9bbdda114e18b66f81c8ecc6550778a6
Jagadeesan M <mjagadeesan113@gmail.com>
2026-03-11 23:07:08 +0530

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from app.views import index, projects, experience, contact
 
 urlpatterns = [
     path('', index, name='index'),
-    path('projects/', projects, name='projects'),
-    path('experience/', experience, name='experience'),
-    path('contact/', contact, name='contact'),
+    path('projects', projects, name='projects'),
+    path('experience', experience, name='experience'),
+    path('contact', contact, name='contact'),
 ]
